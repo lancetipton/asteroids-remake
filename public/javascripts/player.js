fb0a69ca4135262d0ca4508b5bf4137c05dfac1a
lancetipton04 <lancetipton04@gmail.com>
2015-01-13 19:23:56 -0800

    inital commit before convert to JS OOP

---
+++ public/javascripts/player.js
@@ -0,0 +1,19 @@
+function Player(playerInfo){
+  this.id = playerInfo['id'],
+  this.avatar = playerInfo['avatar'],
+  this.paddleLoad = playerInfo['paddle'],
+  this.spriteName = playerInfo['spriteName'],
+  this.points = playerInfo['points'],
+  this.hud = playerInfo['hud'],
+  this.moveUp = playerInfo['moveUp'],
+  this.moveDown = playerInfo['moveDown']
+}
+
+
+// Player.prototype.movement = function(){
+
+// }
+
+// id, avatar, paddle, spriteName, moveUp, moveDown
+
+
